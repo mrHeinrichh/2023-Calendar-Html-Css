317034a8a9a7999a31992c7bc9d050f9e8577f81
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2022-10-10 13:26:33 +0800

    Update web php routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/january', function () {
+    return view('january');
+});
+Route::get('/february', function () {
+    return view('february');
+});
+Route::get('/march', function () {
+    return view('march');
+});
+Route::get('/april', function () {
+    return view('april');
+});
+Route::get('/may', function () {
+    return view('may');
+});
+Route::get('/june', function () {
+    return view('june');
+});
+Route::get('/july', function () {
+    return view('july');
+});
+Route::get('/august', function () {
+    return view('august');
+});
+Route::get('/september', function () {
+    return view('september');
+});
+Route::get('/october', function () {
+    return view('october');
+});
+Route::get('/november', function () {
+    return view('november');
+});
+Route::get('/december', function () {
+    return view('december');
 });
